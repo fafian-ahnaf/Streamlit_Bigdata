b5602e24da9276123beb44deb013d322e5de3abb
fafian-ahnaf <118112409+fafian-ahnaf@users.noreply.github.com>
2025-05-27 15:01:22 +0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 from datetime import datetime
 
 # Koneksi ke MongoDB
-client = MongoClient('mongodb+srv://capstone:artano050904@capstone.ytuhc2r.mongodb.net/')
+client = MongoClient('mongodb+srv://fafianahnaf2003:Fafian2003@cluster0.a1ba9ks.mongodb.net/')
 db = client['Tenis_Meja']  # Gantilah spasi dengan underscore
 collection = db['berita_tenismeja']
 
